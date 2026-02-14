0deb3fcdc6aaf3d36e7833d0a377b613e827eac1
flodardev <63950579+flodardev@users.noreply.github.com>
2026-02-14 11:46:07 +0800

    Added homepage sections

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Restaurant Page
+
+# Credits
+
+- https://pixabay.com/photos/sabah-mountain-heritage-travel-5392642/
+- https://www.pexels.com/photo/smiling-waitress-serving-pizza-to-customer-at-cafe-28945097/
--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -2,12 +2,14 @@ export { homePage };
 
 import "./home-page.css";
 import headlineImage from "../images/dining-area.jpg";
+import storyImage from "../images/kinabalu.jpg";
+import operatingImage from "../images/waitress.jpg";
 
 const homePage = () => {
   // Append to div
   const homeWrapper = document.createElement("div");
   homeWrapper.classList.add("home-wrapper");
-  homeWrapper.append(headlineSection(), storySection());
+  homeWrapper.append(headlineSection(), storySection(), operatingSection());
   return homeWrapper;
 };
 
@@ -39,17 +41,47 @@ const headlineSection = () => {
 
 const storySection = () => {
   // STORY
-  const storyCard = document.createElement("div");
+
+  const storyImg = document.createElement("img");
+  storyImg.src = storyImage;
+
   const homeStory = document.createElement("p");
   homeStory.textContent =
     "Rasa Sabah brings the heart of Borneo to your plate. From the misty peaks of Mount Kinabalu to the azure waters of Sipadan, we source the finest local ingredients to create an authentic Sabahan culinary journey.";
 
-  storyCard.append(homeStory);
+  const storyCard = document.createElement("div");
+  storyCard.classList.add("story-card");
+  storyCard.append(homeStory, storyImg);
   return storyCard;
 };
 
 const operatingSection = () => {
   // OPERATING HOURS
+  const operatingImg = document.createElement("img");
+  operatingImg.src = operatingImage;
+
+  const operatingHours = document.createElement("div");
   const homeOpHours = document.createElement("p");
   homeOpHours.textContent = "Operating Hours";
+  const hourList = document.createElement("ul");
+
+  const hours = [
+    { day: "Monday", time: "Closed" },
+    { day: "Tuesday - Thursday", time: "11:00 AM - 9:00 PM" },
+    { day: "Monday", time: "11:00 AM - 11:00 PM" },
+    { day: "Monday", time: "10:00 AM - 8:00 PM" },
+  ];
+
+  hours.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${item.day}: ${item.time}`;
+    hourList.append(listItem);
+  });
+
+  operatingHours.append(homeOpHours, hourList);
+
+  const opHrsCard = document.createElement("div");
+  opHrsCard.classList.add("operating-card");
+  opHrsCard.append(operatingImg, operatingHours);
+  return opHrsCard;
 };
