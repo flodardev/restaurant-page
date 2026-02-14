488fdc05f71707fb0160cf6e6b18e15fb94b13c6
flodardev <63950579+flodardev@users.noreply.github.com>
2026-02-14 11:00:59 +0800

    Add sections to home page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import "./modern-css-reset.css";
 import "./index.css";
-import { homeSection } from "./pages/home-page.js";
+import { homePage } from "./pages/home-page.js";
 import { menuSection } from "./pages/menu-page.js";
 import { aboutSection } from "./pages/about-page.js";
 
 const mainContainer = document.querySelector("#main-container");
 
+// Default page
+mainContainer.append(homePage());
+
 const homeButton = document.querySelector("#home-btn");
 homeButton.addEventListener("click", () => {
   clearSection();
-  mainContainer.append(homeSection());
+  mainContainer.append(homePage());
 });
 
 const menuButton = document.querySelector("#menu-btn");
--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,8 +1,55 @@
-export { homeSection };
+export { homePage };
 
-const homeSection = () => {
-  const text = "This is the homepage";
-  const homeDiv = document.createElement("div");
-  homeDiv.textContent = text;
-  return homeDiv;
+import "./home-page.css";
+import headlineImage from "../images/dining-area.jpg";
+
+const homePage = () => {
+  // Append to div
+  const homeWrapper = document.createElement("div");
+  homeWrapper.classList.add("home-wrapper");
+  homeWrapper.append(headlineSection(), storySection());
+  return homeWrapper;
+};
+
+const headlineSection = () => {
+  // HEADLINE CARD
+  const headlineCard = document.createElement("div");
+  headlineCard.classList.add("headline-card");
+
+  // title
+  const headlineTitle = document.createElement("h1");
+  headlineTitle.textContent = "Rasa Sabah";
+
+  // headline text
+  const headlineText = document.createElement("p");
+  headlineText.textContent =
+    "Experience the Flavors of the Land Below the Wind";
+
+  const titleCard = document.createElement("div");
+  titleCard.append(headlineTitle, headlineText);
+  titleCard.classList.add("title-card");
+
+  // headline Img
+  const headlineImg = document.createElement("img");
+  headlineImg.src = headlineImage;
+
+  headlineCard.append(titleCard, headlineImg);
+  return headlineCard;
+};
+
+const storySection = () => {
+  // STORY
+  const storyCard = document.createElement("div");
+  const homeStory = document.createElement("p");
+  homeStory.textContent =
+    "Rasa Sabah brings the heart of Borneo to your plate. From the misty peaks of Mount Kinabalu to the azure waters of Sipadan, we source the finest local ingredients to create an authentic Sabahan culinary journey.";
+
+  storyCard.append(homeStory);
+  return storyCard;
+};
+
+const operatingSection = () => {
+  // OPERATING HOURS
+  const homeOpHours = document.createElement("p");
+  homeOpHours.textContent = "Operating Hours";
 };
